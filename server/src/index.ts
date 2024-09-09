9c9320352ebce07c1ba5161b31c9e2d833ef61fa
vunt <vunguyentien10@gmail.com>
2024-09-09 23:38:41 +0700

    test auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,20 +8,21 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import usersRouter from '~/routes/user.routes';
 import blogsRouter from '~/routes/blog.routes';
+import authUserRouter from '~/routes/authUser.routes';
+import { defaultErrorHandler } from '~/middlewares/error.middleware';
 
 const app: Express = express();
 const PORT = envConfig.PORT;
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false // Disable the `X-RateLimit-*` headers
   // store: ... , // Use an external store for more precise rate limiting
 });
 
-connectDB().then(() => {
-});
+connectDB().then(() => {});
 
 app.set('trust proxy', 1); // trust first proxy
 
@@ -37,8 +38,11 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello word');
 });
 
+app.use('/api/auth/users', authUserRouter);
 app.use('/users', usersRouter);
 app.use('/blogs', blogsRouter);
+
+app.use(defaultErrorHandler);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
