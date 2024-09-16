40ee00b32ca46c07b837e642d84760268a46e672
vunt <vunguyentien10@gmail.com>
2024-09-16 12:49:33 +0700

    login and register

--- client/src/useRouteElement.jsx
+++ client/src/useRouteElement.jsx
@@ -1,17 +1,46 @@
 import { useRoutes } from 'react-router-dom';
 import Home from './pages/Home';
 import HomeLanding from './pages/HomeLanding';
+import { Suspense } from 'react';
+import AuthLayout from './layouts/AuthLayout/index.js';
+import Login from './pages/Login/index.js';
+import Register from './pages/Register/index.js';
 
 export default function useRouteElement() {
   return useRoutes([
     {
       path: '/',
       index: true,
-      element: <HomeLanding />
+      element: (
+        <Suspense>
+          <HomeLanding />
+        </Suspense>
+      )
     },
     {
       path: '/home',
       element: <Home />
+    },
+    {
+      path: 'login',
+
+      element: (
+        <AuthLayout>
+          <Suspense>
+            <Login />
+          </Suspense>
+        </AuthLayout>
+      )
+    },
+    {
+      path: 'register',
+      element: (
+        <AuthLayout>
+          <Suspense>
+            <Register />
+          </Suspense>
+        </AuthLayout>
+      )
     }
   ]);
 }
